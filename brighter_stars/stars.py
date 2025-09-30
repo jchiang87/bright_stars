fa036065390904c37fb82486a7ae625c0c093c15
Jim Chiang <jchiang@slac.stanford.edu>
2025-09-30 11:13:43 -0700

    add stars module

---
+++ brighter_stars/stars.py
@@ -0,0 +1,63 @@
+"""
+Classes to wrap skycatalogs.StarObjects to enable their fluxes to
+be increased by a delta magnitude value set in the yaml config.
+"""
+import galsim
+from skycatalogs.objects import BaseObject, ObjectCollection
+
+__all__ = ["BrighterStarsObject", "BrighterStarsCollection"]
+
+
+class BrighterStarsObject(BaseObject):
+
+    def __init__(self, star_object, delta_magnorm):
+        self.star_object = star_object
+        self.delta_magnorm = delta_magnorm
+
+    def get_gsobject_components(self, gsparams=None, rng=None):
+        if gsparams is not None:
+            gsparams = galsim.GSParams(**gsparams)
+        return {'this_object': galsim.DeltaFunction(gsparams=gsparams)}
+
+    def get_observer_sed_component(self, component, mjd=None):
+        if component != "this_object":
+            raise RuntimeError("Unknown SED component: %s", component)
+        sed = self.star_object.get_observer_sed_component(component, mjd=mjd)
+        # Apply delta_magnorm
+        sed *= 10.0**(-self.delta_magnorm/2.5)
+        return sed
+
+
+class BrighterStarsCollection(ObjectCollection):
+    _object_type = "brighter_stars"
+
+    def __init__(self, collection_list, delta_magnorm):
+        self.collection_list = collection_list
+        self.delta_magnorm = delta_magnorm
+
+    @property
+    def native_columns(self):
+        return ()
+
+    def __getitem__(self, key):
+        results = self.collection_list.__getitem__(key)
+        return [BrighterStarsObject(_, self.delta_magnorm) for _ in results]
+
+    def __len__(self):
+        return self.collection_list._total_len
+
+    @staticmethod
+    def register(sky_catalog, object_type):
+        sky_catalog.cat_cxt.register_source_type(
+            BrighterStarsCollection._object_type,
+            object_class=BrighterStarsObject,
+            collection_class=BrighterStarsCollection,
+            custom_load=True
+        )
+
+    @staticmethod
+    def load_collection(region, sky_catalog, mjd=None, **kwds):
+        object_type = BrighterStarsCollection._object_type
+        config = dict(sky_catalog.raw_config["object_types"][object_type])
+        collection_list = sky_catalog.get_object_type_by_region(region, "star")
+        return BrighterStarsCollection(collection_list, config['delta_magnorm'])
